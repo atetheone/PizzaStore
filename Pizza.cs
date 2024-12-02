bb6fd3861d790ce95e5dd674717c9658fda96a8f
atetheone <atevirran@gmail.com>
2024-12-02 01:47:58 +0000

    Add the second class PizzaDb

--- Pizza.cs
+++ Pizza.cs
@@ -8,5 +8,11 @@ namespace PizzaStore.Models
 		public string? Nom { get; set; }
 		public string? Description { get; set; }
 	}
-
+	
+	// Classe PizzaDb qui représente le contexte de base de données
+	public class PizzaDb : DbContext
+	{
+		public PizzaDb(DbContextOptions<PizzaDb> options) : base(options) { }
+		public DbSet<Pizza> Pizzas { get; set; } = null!;
+	}
 }
